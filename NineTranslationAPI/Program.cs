3f522cd1927bd6c96c90824e1214bb8caa34af45
UselessWeeb <sansunderswap2004@gmail.com>
2025-07-25 20:38:25 +0700

    update cors

--- NineTranslationAPI/Program.cs
+++ NineTranslationAPI/Program.cs
@@ -87,7 +87,7 @@ builder.Services.AddCors(options =>
     options.AddPolicy("AllowReactApp",
         policy =>
         {
-            policy.WithOrigins("http://localhost:3002")
+            policy.WithOrigins("http://localhost:3002", "http://localhost:3000")
                   .AllowAnyHeader()
                   .AllowAnyMethod()
                   .AllowCredentials();
